8c688b9f6fdefb8c5e65034d1d5c00d1ba414e51
Maksim <kma-snz@mail.ru>
2026-05-11 11:35:11 +0500

    wip

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-
+  build: {
+    outDir: 'dist',
+  },
   server: {
     host: true,           // разрешает доступ с 0.0.0.0 и localhost
     port: 5173,           // явно указываем порт
